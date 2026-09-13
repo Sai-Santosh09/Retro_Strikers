41d2593c31f9ca4c6b256437707986f81bacb985
Sai-Santosh09 <saisantosh2712@gmail.com>
2026-09-13 23:27:20 +0530

    added the ability to click pass and the nearest player to the ball becomes the real player

--- scenes/container.gd
+++ scenes/container.gd
@@ -45,8 +45,9 @@ func spawn_players( country : String, goal : Goal ) -> Array[ Player ]:
 
 
 func spawn_player( player_position : Vector2, goal : Goal, target_goal : Goal, player_data : PlayerResource, country : String ) -> Player:
-	var player := PLAYER_PREFAB.instantiate()
+	var player : Player = PLAYER_PREFAB.instantiate()
 	player.initialize( player_position, ball, goal , target_goal, player_data, country )
+	player.swap_requested.connect( on_player_swap_requested.bind() )
 	return player
 
 
@@ -60,3 +61,20 @@ func set_on_duty_weights() -> void:
 			)
 		for i in range( cpu_players.size() ):
 			cpu_players[ i ].weight_on_duty_steering = 1 - ease( float(i) / 10.0, 0.1 )
+
+
+func on_player_swap_requested( requester : Player ) -> void:
+	var squad := squad_left if requester.country == squad_left[0].country else squad_right
+	var cpu_players : Array[ Player ] = squad.filter(
+			func( p : Player ) : return p.control_scheme == Player.ControlScheme.CPU and p.role != Player.Role.GOALIE
+		)
+	cpu_players.sort_custom( func( p1 : Player, p2 : Player ):
+		return p1.position.distance_squared_to( ball.position ) < p2.position.distance_squared_to( ball.position ) 
+		)
+	var closest_cpu_to_ball : Player = cpu_players[0]
+	if closest_cpu_to_ball.position.distance_squared_to( ball.position ) < requester.position.distance_squared_to( ball.position ):
+		var player_control_scheme := requester.control_scheme
+		requester.control_scheme = Player.ControlScheme.CPU
+		requester.set_control_texture()
+		closest_cpu_to_ball.control_scheme = player_control_scheme
+		closest_cpu_to_ball.set_control_texture()
--- scenes/states/player_state_moving.gd
+++ scenes/states/player_state_moving.gd
@@ -16,15 +16,17 @@ func handle_human_movement() -> void:
 	if player.velocity != Vector2.ZERO:
 		teammate_detection_area.rotation = player.velocity.angle()
 	
-	if player.has_ball():
-		if KeyUtil.is_action_just_pressed(player.control_scheme, KeyUtil.Action.PASS):
-			transition_state(Player.State.PASSING)
-		elif KeyUtil.is_action_just_pressed(player.control_scheme, KeyUtil.Action.SHOOT):
-			transition_state(Player.State.PREPPING_SHOT)
-	elif can_teammate_pass_ball() and KeyUtil.is_action_just_pressed( player.control_scheme, KeyUtil.Action.PASS ):
-		ball.carrier.get_pass_request( player )
+	if KeyUtil.is_action_just_pressed( player.control_scheme, KeyUtil.Action.PASS ):
+		if player.has_ball():
+			transition_state( Player.State.PASSING )
+		elif can_teammate_pass_ball():
+			ball.carrier.get_pass_request( player )
+		else:
+			player.swap_requested.emit( player )
 	elif KeyUtil.is_action_just_pressed( player.control_scheme, KeyUtil.Action.SHOOT ):
-		if ball.can_air_interact():
+		if player.has_ball():
+			transition_state( Player.State.PREPPING_SHOT )
+		elif ball.can_air_interact():
 			if player.velocity == Vector2.ZERO:
 				if player.is_facing_target_goal():
 					transition_state( Player.State.VOLLEY_KICK )
